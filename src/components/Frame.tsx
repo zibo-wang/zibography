2c5882fead01424095dc2940122a32985aa74817
Zibo Wang <zibo.w@outlook.com>
2023-12-04 15:00:21 +1100

    update menu

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 const Frame: React.FC = () => {
     return (
         <div className="frame">
-            <h1 className="frame__title">Scroll Loop Menu</h1>
+            <h1 className="frame__title">Zibography</h1>
             <div className="frame__links">
                 <a href="https://tympanus.net/codrops/?p=49748" className="frame__link">Article</a>
                 <a href="https://github.com/codrops/ScrollLoopMenu/" className="frame__link">GitHub</a>
             </div>
             <span className="frame__button" aria-hidden="true">Close</span>
+            <span className="frame__footer" aria-hidden="true">Minimize</span>
         </div>
     );
 };
